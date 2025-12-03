b64c070bc67d9b494a2ba66257d006b29e2c7cd8
Albert Vallverdu <AV+owgtm@owgtm.com>
2025-12-03 21:21:31 +0000

    add @constants to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@constants': path.resolve(__dirname, './src/constants'),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './setupTests.tsx',
     css: true,
   },
 });
