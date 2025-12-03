6b6622f5b62b2b5c0091a4f93c7dc889c6ef1091
Albert Vallverdu <byverdu@gmail.com>
2025-12-03 23:57:00 +0000

    add chunk method tests

--- src/utils/__tests__/chunk.spec.ts
+++ src/utils/__tests__/chunk.spec.ts
@@ -7,8 +7,32 @@ describe('chunk', () => {
     expect(chunk).instanceOf(Function);
   });
 
-  test('should return empty array', () => {
-    expect(chunk(null)).toEqual([]);
-    expect(chunk([undefined], 0)).toEqual([]);
+  test.each([
+    [null, []],
+    [undefined, []],
+    [{}, []],
+    [true, []],
+    [4, []],
+  ])('returns empty array for %o', (input, expected) => {
+    expect(chunk(input)).toEqual(expected);
+  });
+
+  test.each([
+    {
+      input: { arr: [1, 2, 3, 4, 5], size: 2 },
+      expected: [[1, 2], [3, 4], [5]],
+    },
+    {
+      input: { arr: ['a', 'b', 'c', 'd'], size: 3 },
+      expected: [['a', 'b', 'c'], ['d']],
+    },
+    {
+      input: { arr: [1, 2, 3, 4, 5], size: 1 },
+      expected: [[1], [2], [3], [4], [5]],
+    },
+    { input: { arr: [1, 2, 3], size: 5 }, expected: [[1, 2, 3]] },
+    { input: { arr: [], size: 2 }, expected: [] },
+  ])('splits array $input.arr by $input.size', ({ input, expected }) => {
+    expect(chunk(input.arr, input.size)).toEqual(expected);
   });
 });
