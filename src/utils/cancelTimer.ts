cd6b00a66dc951adfb9254ec9388d465db8eb6aa
Albert Vallverdu <byverdu@gmail.com>
2025-12-09 08:15:46 +0000

    wip cancelTimer

--- src/utils/cancelTimer.ts
+++ src/utils/cancelTimer.ts
@@ -1,29 +1,76 @@
 type TimerType = 'setTimeout' | 'setInterval';
 
-/**
- * Cancels a timeout or interval before it executes.
- *
- * @param {(args: Array<unknown>) => void} callback - The function to invoke after the delay.
- * @param {"setTimeout" | "setInterval"} timerType - What JS timer to use
- * @param {number} delay - The delay in milliseconds before the callback is invoked.
- * @param {...Array<unknown>} args - Arguments to pass to the callback function.
- * @returns {() => void} A function that, when called, clears the timeout.
- */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Args = (...args: any[]) => any;
 
-export function cancelTimer(
-  timerType: TimerType,
-  callback: (args: Array<unknown>) => void,
-  delay: number,
-  ...args: Array<unknown>
+type Props<T extends Args> = {
+  callback: T;
+  timerType: TimerType;
+  delay: number;
+};
+
+/**
+ * Starts a timeout or interval, and returns a function to cancel it before execution.
+ *
+ * @param params Parameters for the timer.
+ * @param params.callback The function to invoke after the delay.
+ * @param params.timerType Type of JS timer to use: 'setTimeout' or 'setInterval'.
+ * @param params.delay The delay in milliseconds before the callback is invoked or interval between invocations.
+ * @param params._args Arguments to pass to the callback function (as an array).
+ * @returns A function that, when called, clears the timeout or interval.
+ *
+ * @example
+ * // Example 1: setTimeout usage
+ * const cancel = cancelTimer<(a: number, b: number) => number>({
+ *   timerType: 'setTimeout',
+ *   callback: (a, b) => a + b,
+ *   delay: 1000
+ * }, 1, 2);
+ * // To cancel before execution:
+ * // cancel();
+ *
+ * @example
+ * // Example 2: setInterval usage
+ * const cancelInterval = cancelTimer({
+ *   timerType: 'setInterval',
+ *   callback: ([msg]) => console.log(msg),
+ *   delay: 500,
+ *   _args: ['Repeated message']
+ * });
+ * // To cancel after 2 seconds:
+ * // setTimeout(cancelInterval, 2000);
+ *
+ */
+export function cancelTimer<T extends Args>(
+  { timerType, callback, delay }: Props<T>,
+  ...args: Parameters<T>
 ) {
   const execTimer = timerType === 'setInterval' ? setInterval : setTimeout;
   const clearTimer = timerType === 'setInterval' ? clearInterval : clearTimeout;
 
   const timer = execTimer(() => {
-    callback(args);
+    callback(...args);
   }, delay);
 
   return () => {
     clearTimer(timer);
   };
 }
+
+type Temp = (a: number, b: number, c: number) => number;
+
+const x = cancelTimer<Temp>(
+  {
+    timerType: 'setTimeout',
+    delay: 100,
+    callback: (a, b) => {
+      console.log(a + b);
+      return a + b;
+    },
+  },
+  1,
+  3,
+  5,
+);
+
+setTimeout(x, 2000);
